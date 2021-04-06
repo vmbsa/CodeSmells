1ce1ec717341017dffbfa48f9b14642be1dc3059
OmegaP1 <J.veloso.00@gmail.com>
2021-04-06 10:23:10 +0100

    Gui_Done

---
+++ ES-2Sem-2021-Grupo-8/src/fronted/Gui_Init.java
@@ -0,0 +1,147 @@
+package FrontEnd;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.border.TitledBorder;
+
+public class Gui_Init {
+
+	private JFrame frame;
+	private JTextPane txtpnAsdsa = new JTextPane();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui_Init window = new Gui_Init();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Gui_Init() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1227, 690);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(Color.WHITE);
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(
+				"logo_ISCTE-IUL.jpg"));
+
+		JLabel lblNewLabel_1 = new JLabel("Projeto ES ");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 40));
+
+		JButton btnNewButton = new JButton("Selecione um projeto java");
+		btnNewButton.setFocusable(false);
+		btnNewButton.setBorderPainted(false);
+		btnNewButton.setBorder(null);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBackground(new Color(0, 120, 215));
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JFileChooser fc = new JFileChooser();
+				int returnVal = fc.showOpenDialog(fc);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					System.out.print("Opening: " + file.getName() + ".");
+					txtpnAsdsa.setText(file.getName());
+				}
+			}
+		});
+		txtpnAsdsa.setEditable(false);
+		txtpnAsdsa.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+
+		txtpnAsdsa.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
+		JButton btnNewButton_1 = new JButton("Extrair Metodos");
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Gui_Metricas gui_metrics = new Gui_Metricas();
+				gui_metrics.setVisible(true);
+			}
+		});
+		btnNewButton_1.setBackground(SystemColor.textHighlight);
+		btnNewButton_1.setBorder(null);
+
+		JButton btnNewButton_1_1 = new JButton("Definir Regras");
+		btnNewButton_1_1.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Gui_Regras gui_regras = new Gui_Regras();
+				gui_regras.setVisible(true);
+			}
+		});
+		btnNewButton_1_1.setBorder(null);
+		btnNewButton_1_1.setBackground(SystemColor.textHighlight);
+
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addContainerGap(136, Short.MAX_VALUE)
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup().addComponent(lblNewLabel).addGap(115))
+								.addGroup(groupLayout.createSequentialGroup().addComponent(lblNewLabel_1).addGap(548))))
+				.addGroup(
+						groupLayout.createSequentialGroup().addGap(359)
+								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(btnNewButton_1_1, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 188, Short.MAX_VALUE))
+								.addGap(18)
+								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(txtpnAsdsa, GroupLayout.DEFAULT_SIZE, 498, Short.MAX_VALUE))
+								.addContainerGap(148, Short.MAX_VALUE)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
+				.createSequentialGroup().addGap(23)
+				.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 302, Short.MAX_VALUE).addGap(28)
+				.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE).addGap(99)
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false).addComponent(txtpnAsdsa)
+						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE))
+				.addGap(18)
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton_1_1, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE))
+				.addGap(51)));
+		frame.getContentPane().setLayout(groupLayout);
+
+	}
+}
---
+++ ES-2Sem-2021-Grupo-8/src/fronted/Gui_Metricas.java
@@ -0,0 +1,149 @@
+package FrontEnd;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.SystemColor;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+
+public class Gui_Metricas extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui_Metricas window = new Gui_Metricas();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Gui_Metricas() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+
+		getContentPane().setBackground(SystemColor.text);
+
+		JLabel lblNewLabel = new JLabel("N\u00FAmero de packages");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JLabel lblNmeroDeClasses = new JLabel("N\u00FAmero de classes");
+		lblNmeroDeClasses.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JLabel lblNmeroDeMtodos = new JLabel("N\u00FAmero de m\u00E9todos");
+		lblNmeroDeMtodos.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JLabel lblNmeroTotalDe = new JLabel("N\u00FAmero total de linhas de c\u00F3digo");
+		lblNmeroTotalDe.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_1 = new JLabel("0");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_1_1 = new JLabel("0");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_1_2 = new JLabel("0");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_1_3 = new JLabel("0");
+		lblNewLabel_1_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+		JTextArea textArea = new JTextArea();
+		textArea.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+
+		JLabel lblNewLabel_2 = new JLabel("M\u00E9tricas");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addGap(457).addComponent(lblNewLabel_2)
+						.addContainerGap(53, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup().addContainerGap(66, Short.MAX_VALUE)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNmeroDeClasses, GroupLayout.PREFERRED_SIZE, 330,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNmeroDeMtodos, GroupLayout.PREFERRED_SIZE, 330,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNmeroTotalDe, GroupLayout.PREFERRED_SIZE, 330,
+										GroupLayout.PREFERRED_SIZE))
+						.addGap(112)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel_1_3, GroupLayout.PREFERRED_SIZE, 44,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 44,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 44,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 44,
+										GroupLayout.PREFERRED_SIZE))
+						.addGap(148).addComponent(textArea, GroupLayout.PREFERRED_SIZE, 464, GroupLayout.PREFERRED_SIZE)
+						.addGap(47)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+				groupLayout.createSequentialGroup().addGap(37)
+						.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
+						.addGap(108).addGroup(groupLayout
+								.createParallelGroup(Alignment.LEADING)
+								.addGroup(
+										Alignment.TRAILING,
+										groupLayout.createSequentialGroup().addGap(8)
+												.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 231,
+														GroupLayout.PREFERRED_SIZE))
+								.addGroup(groupLayout.createSequentialGroup().addGroup(groupLayout
+										.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblNewLabel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(lblNewLabel, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 54,
+												GroupLayout.PREFERRED_SIZE))
+										.addPreferredGap(ComponentPlacement.UNRELATED)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addGroup(groupLayout.createSequentialGroup()
+														.addComponent(lblNmeroDeClasses, GroupLayout.PREFERRED_SIZE, 54,
+																GroupLayout.PREFERRED_SIZE)
+														.addPreferredGap(ComponentPlacement.RELATED)
+														.addComponent(lblNmeroDeMtodos, GroupLayout.PREFERRED_SIZE, 54,
+																GroupLayout.PREFERRED_SIZE)
+														.addPreferredGap(ComponentPlacement.RELATED)
+														.addComponent(lblNmeroTotalDe, GroupLayout.PREFERRED_SIZE, 54,
+																GroupLayout.PREFERRED_SIZE))
+												.addGroup(groupLayout.createSequentialGroup()
+														.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 54,
+																GroupLayout.PREFERRED_SIZE)
+														.addPreferredGap(ComponentPlacement.RELATED)
+														.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 54,
+																GroupLayout.PREFERRED_SIZE)
+														.addPreferredGap(ComponentPlacement.RELATED)
+														.addComponent(lblNewLabel_1_3, GroupLayout.PREFERRED_SIZE, 54,
+																GroupLayout.PREFERRED_SIZE)))))
+						.addGap(158)));
+		getContentPane().setLayout(groupLayout);
+		setBackground(SystemColor.text);
+		setBounds(100, 100, 1227, 690);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
