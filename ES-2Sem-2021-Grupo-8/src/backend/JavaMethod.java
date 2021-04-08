dda65bd17fb4fd2b294c422f117d43b12ae17aa3
ASUS <ASUS@DESKTOP-RMON4EF>
2021-04-08 20:20:18 +0100

    LOC_method extraction

--- ES-2Sem-2021-Grupo-8/src/backend/JavaMethod.java
+++ ES-2Sem-2021-Grupo-8/src/backend/JavaMethod.java
@@ -6,14 +6,24 @@ public class JavaMethod {
 
 	private String name;
 	private MethodDeclaration md;
+	private int size;
 	
 	public JavaMethod(String name, MethodDeclaration md) {
 		this.name = name;
 		this.md = md;
+		getLOCMethod();
 	}
 
 	public String getName() {
 		return name;
 	}
+	
+	private void getLOCMethod() {
+		String[] lines = md.getBody().toString().split("\r\n|\r|\n");
+		size = lines.length;
+		//apagar depois de verem que funcemina 
+		System.out.println("metodo " + name + " tem " + size + " linhas");
+	}
+	
 	
 }
