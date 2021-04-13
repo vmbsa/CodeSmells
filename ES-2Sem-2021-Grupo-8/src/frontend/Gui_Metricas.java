278d2474b1b76387621de9da4231301957bac1e8
OmegaP1 <J.veloso.00@gmail.com>
2021-04-13 13:03:25 +0100

    Excel_Finished

--- ES-2Sem-2021-Grupo-8/src/frontend/Gui_Metricas.java
+++ ES-2Sem-2021-Grupo-8/src/frontend/Gui_Metricas.java
@@ -15,12 +15,13 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.table.DefaultTableModel;
 
 import backend.Excel_Helper;
+import java.awt.Color;
 
 public class Gui_Metricas extends JFrame {
 
@@ -60,30 +61,6 @@ public class Gui_Metricas extends JFrame {
 
 		getContentPane().setBackground(SystemColor.text);
 
-		JLabel lblNewLabel = new JLabel("N\u00FAmero de packages");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JLabel lblNmeroDeClasses = new JLabel("N\u00FAmero de classes");
-		lblNmeroDeClasses.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JLabel lblNmeroDeMtodos = new JLabel("N\u00FAmero de m\u00E9todos");
-		lblNmeroDeMtodos.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JLabel lblNmeroTotalDe = new JLabel("N\u00FAmero total de linhas de c\u00F3digo");
-		lblNmeroTotalDe.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JLabel lblNewLabel_1 = new JLabel("0");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JLabel lblNewLabel_1_1 = new JLabel("0");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JLabel lblNewLabel_1_2 = new JLabel("0");
-		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JLabel lblNewLabel_1_3 = new JLabel("0");
-		lblNewLabel_1_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
 		JLabel lblNewLabel_2 = new JLabel("M\u00E9tricas");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 40));
 
@@ -99,9 +76,6 @@ public class Gui_Metricas extends JFrame {
 					Excel_Helper eh = new Excel_Helper(excelPath);
 					ArrayList<String> helper=eh.readExcelSheet(true);
 					System.out.println(helper.get(0));
-					
-
-					
 					DefaultTableModel dtm = new DefaultTableModel();
 					dtm.addColumn("MethodID");
 					dtm.addColumn("package");
@@ -122,7 +96,6 @@ public class Gui_Metricas extends JFrame {
 								,abc[7],(int) Double.parseDouble(abc[8]),(int) Double.parseDouble(abc[9]),abc[10]});
 					}
 					tableExcel.setModel(dtm);
-
 				}
 			}
 		});
@@ -130,92 +103,111 @@ public class Gui_Metricas extends JFrame {
 		btnVerExcel.setBorderPainted(false);
 		btnVerExcel.setBorder(null);
 		btnVerExcel.setBackground(SystemColor.textHighlight);
-
+		
 		JPanel panel = new JPanel();
+		panel.setBorder(null);
+		panel.setBackground(Color.WHITE);
+		
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(457)
+					.addGap(486)
 					.addComponent(lblNewLabel_2)
-					.addContainerGap(609, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(56)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNmeroDeClasses, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNmeroDeMtodos, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNmeroTotalDe, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE))
-					.addGap(112)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel_1_3, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))
-					.addGap(170)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE)
-						.addComponent(btnVerExcel, GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE))
-					.addGap(47))
+					.addContainerGap(47, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGap(76)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 532, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(btnVerExcel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 528, Short.MAX_VALUE))
+					.addGap(25))
 		);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(37)
+					.addGap(23)
 					.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
-					.addGap(114)
+					.addGap(47)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 54, Short.MAX_VALUE)
-								.addComponent(lblNewLabel, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblNmeroDeClasses, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(lblNmeroDeMtodos, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(lblNmeroTotalDe, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(lblNewLabel_1_3, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)))
-							.addGap(79))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 254, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)))
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 252, GroupLayout.PREFERRED_SIZE)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 254, GroupLayout.PREFERRED_SIZE))
+					.addGap(94)
 					.addComponent(btnVerExcel, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-					.addGap(32))
+					.addGap(83))
 		);
-
+		
 		tableExcel = new JTable();
+		scrollPane.setViewportView(tableExcel);
 		
-		JScrollBar scrollBar = new JScrollBar();
+				JLabel lblNewLabel = new JLabel("N\u00FAmero de packages");
+				lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		
-		JScrollBar scrollBar_1 = new JScrollBar();
-		scrollBar_1.setOrientation(JScrollBar.HORIZONTAL);
+				JLabel lblNmeroDeClasses = new JLabel("N\u00FAmero de classes");
+				lblNmeroDeClasses.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+				JLabel lblNmeroDeMtodos = new JLabel("N\u00FAmero de m\u00E9todos");
+				lblNmeroDeMtodos.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+				JLabel lblNmeroTotalDe = new JLabel("N\u00FAmero total de linhas de c\u00F3digo");
+				lblNmeroTotalDe.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+				JLabel lblNewLabel_1_3 = new JLabel("0");
+				lblNewLabel_1_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+				JLabel lblNewLabel_1_2 = new JLabel("0");
+				lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+				JLabel lblNewLabel_1_1 = new JLabel("0");
+				lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+				JLabel lblNewLabel_1 = new JLabel("0");
+				lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(tableExcel, GroupLayout.DEFAULT_SIZE, 419, Short.MAX_VALUE)
-						.addComponent(scrollBar_1, GroupLayout.DEFAULT_SIZE, 419, Short.MAX_VALUE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNmeroDeClasses, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNmeroDeMtodos, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNmeroTotalDe, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 330, GroupLayout.PREFERRED_SIZE))
+					.addGap(73)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1_3, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))
+					.addGap(75))
 		);
 		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.TRAILING)
-				.addComponent(scrollBar, GroupLayout.DEFAULT_SIZE, 272, Short.MAX_VALUE)
+			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(tableExcel, GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollBar_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNmeroDeClasses, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblNmeroDeMtodos, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addGap(3)))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNmeroTotalDe, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNewLabel_1_3, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(12, Short.MAX_VALUE))
 		);
 		panel.setLayout(gl_panel);
 		getContentPane().setLayout(groupLayout);
