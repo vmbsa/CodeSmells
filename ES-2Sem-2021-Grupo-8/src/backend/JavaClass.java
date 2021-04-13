036e7fd614fffb8b1d2df4b56f157cc56d9fb121
msmsa1-iscte <msmsa1@iscte-iul.pt>
2021-04-13 13:24:49 +0100

    Bugged LOC_Class method

--- ES-2Sem-2021-Grupo-8/src/backend/JavaClass.java
+++ ES-2Sem-2021-Grupo-8/src/backend/JavaClass.java
@@ -1,20 +1,29 @@
 package backend;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.symbolsolver.javaparsermodel.contexts.ClassOrInterfaceDeclarationContext;
+import com.google.common.io.Files;
 
 public class JavaClass extends VoidVisitorAdapter<Void> {
 
 	private String name;
 	private File file;
 	private List<JavaMethod> methods_list = new ArrayList<JavaMethod>();
+	private List<String> string_list = new ArrayList<String>();
+	private int size;
 
 	public JavaClass(String name, File file) {
 		this.name = name;
@@ -25,7 +34,6 @@ public class JavaClass extends VoidVisitorAdapter<Void> {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
 	public String getName() {
@@ -38,6 +46,30 @@ public class JavaClass extends VoidVisitorAdapter<Void> {
 		methods_list.add(new JavaMethod(md.getNameAsString(), md));
 	}
 
+	@Override
+	public void visit(ClassOrInterfaceDeclaration cd, Void arg) {
+		super.visit(cd, arg);
+		size = cd.getRange().map(range -> range.end.line - range.begin.line).orElse(0);
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line;
+			int empty = 0;
+			while ((line = br.readLine()) != null) {
+				if (line.trim().isEmpty()) {
+					empty++;
+				}
+			}
+			size = size - empty + 1;
+			System.out.println(empty);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("A classe " + name + " tem: " + size);
+	}
+
 	private void getMethods() throws FileNotFoundException {
 		final CompilationUnit comp = StaticJavaParser.parse(file);
 		visit(comp, null);
@@ -46,5 +78,5 @@ public class JavaClass extends VoidVisitorAdapter<Void> {
 	public int getNOMClass() {
 		return methods_list.size();
 	}
-  
+
 }
