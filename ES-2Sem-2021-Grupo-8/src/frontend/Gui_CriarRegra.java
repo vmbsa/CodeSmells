f2a8e335e57b1bbf09d3fa100be6d812a75df823
Lourenço Cordeiro <lourencoscordeiro@gmail.com>
2021-05-05 12:14:33 +0100

    Revert "resolving GUI problems part 2"
    This reverts commit 1945df751d95a6fc7f2a2f20f31d3539be6b9e03.

--- ES-2Sem-2021-Grupo-8/src/frontend/Gui_CriarRegra.java
+++ ES-2Sem-2021-Grupo-8/src/frontend/Gui_CriarRegra.java
@@ -44,7 +44,6 @@ public class Gui_CriarRegra extends JFrame {
 	private JTextField regra_nome;
 	private JLabel label;
 
-	private String filename="";
 	private String excel_file_path;
 	private String rules = "";
 	private ArrayList<String> ArrayMethods = new ArrayList<String>();
@@ -57,10 +56,8 @@ public class Gui_CriarRegra extends JFrame {
 	initialize();
 	}
 
-	public String GetRule() {
-		return rules;
-	}
-
+	
+	
 	public void initialize() {
 		setBounds(100, 100, 864, 468);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -88,7 +85,7 @@ public class Gui_CriarRegra extends JFrame {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(SystemColor.text);
 		panel_1.setBorder(new CompoundBorder());
-		
+
 		JButton Button_Adicionar = new JButton("Adicionar");
 		Button_Adicionar.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		Button_Adicionar.setBackground(SystemColor.textHighlight);
@@ -96,7 +93,7 @@ public class Gui_CriarRegra extends JFrame {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 		
-			filename=textField.getText();
+			String filename=textField.getText();
 			
 			try {
 				File myObj = new File("Regras/" + filename + ".txt");
@@ -124,7 +121,8 @@ public class Gui_CriarRegra extends JFrame {
 
 		Button_Remover.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+			
+				;
 			}
 		});
 
@@ -296,8 +294,5 @@ public class Gui_CriarRegra extends JFrame {
 		);
 		panel_1.setLayout(gl_panel_1);
 		getContentPane().setLayout(groupLayout);
-	}
-	public String String_Constructor() {
-		return " " + "Regra: " +filename + " " + GetRule();
 	}
 }
