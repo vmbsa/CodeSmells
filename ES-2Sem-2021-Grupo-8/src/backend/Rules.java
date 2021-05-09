ae6c456ffddeb6730835fbc69bc71d4dd652040b
Ana <Ana@DESKTOP-9COQV82>
2021-05-09 12:48:52 +0100

    starting rules refactoring

--- ES-2Sem-2021-Grupo-8/src/backend/Rules.java
+++ ES-2Sem-2021-Grupo-8/src/backend/Rules.java
@@ -2,36 +2,55 @@ package backend;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
-
-
 
 public class Rules {
 
+	private static final File FOLDER = new File("Regras");
 
-	private static File FOLDERNAME;
-	
-	
-	public Rules() {
-		
-	}
-
-	
-	
-	private void loadRule() {
-		String data=null;
-			try {
-			Scanner myReader= new Scanner(FOLDERNAME);
-			while(myReader.hasNextLine()) {
-				data=myReader.nextLine();
+	public String getRuleByFilename(String filename) {
+		for (File rule_file : FOLDER.listFiles()) {
+			if (rule_file.getName() == filename) {
+				return getFileContent(rule_file);
 			}
-			myReader.close();
-				
-			
-		} catch (FileNotFoundException e1) {
-			System.out.println("An error Occurred");
-			e1.printStackTrace();
+		}
+		return null;
+	}
+	
+	private String getFileContent(File f) {
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(f);
+			if (scanner.hasNextLine()) {
+				String file_data = scanner.nextLine();
+				scanner.close();
+				return file_data;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		scanner.close();
+		return null;
+	}
+	
+	public void writeRule(String file_name, String file_content) {
+		File new_file = new File(FOLDER.getAbsoluteFile() + "//" + file_name);
+		System.out.println(new_file.getAbsolutePath());
+		try {
+			PrintWriter writer = new PrintWriter(new_file);
+			writer.print(file_content);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
+	public static void main(String[] args) {
+		Rules r = new Rules();
+		r.writeRule("Testteeeeeeeeeee.txt", "Isto é um teste");
+	}
+
 }
